9275bb74df380ff1791152aea4747ab71b99e5c9
Dharam <Dharam@Dharam-PC>
2018-05-09 13:38:10 +0530

    First Commit

---
+++ TestNGMavenGitJenkins/src/main/java/TestNGFramework/TestNGMavenGitJenkins/App.java
@@ -0,0 +1,13 @@
+package TestNGFramework.TestNGMavenGitJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
