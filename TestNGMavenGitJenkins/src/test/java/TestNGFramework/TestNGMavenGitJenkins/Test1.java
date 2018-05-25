62fc374b0e10b416cbf421d020fa5ec5187c0d1f
Dharam <Dharam@Dharam-PC>
2018-05-25 20:26:57 +0530

    Adding Logger

--- TestNGMavenGitJenkins/src/test/java/TestNGFramework/TestNGMavenGitJenkins/Test1.java
+++ TestNGMavenGitJenkins/src/test/java/TestNGFramework/TestNGMavenGitJenkins/Test1.java
@@ -1,7 +1,12 @@
 package TestNGFramework.TestNGMavenGitJenkins;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -11,13 +16,24 @@ import com.core.Framework;
 
 public class Test1 {
 	Framework framework=new Framework();
+	Logger Log=null;
+	
 	@BeforeTest
 	public void beforTest() 
 	{
-		
-		
+		FileInputStream file=null;
+		try {
+			file = new FileInputStream(new File("E:\\LatestLocalRepo\\TestNGMavenGitJenkins\\Logs\\Log4j.properties"));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		PropertyConfigurator.configure(file);
+		Log=Logger.getLogger(Test1.class);
 		try {
 			framework.collectEnvironmentData();
+			Log.debug("************************Collecting Envirnonment Data*************************************************");
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -31,19 +47,23 @@ public class Test1 {
 	{
 		
 		framework.driver.quit();
+		Log.debug("************************Browser Closed*************************************************");
 	}
 	@Test
 	public void test1()
 	{
+		Log.debug("************************test1 execution Start*********************************************");
 		Assert.assertEquals(true, true);
+		Log.debug("************************test1 execution Complete*************************************************");
 		
 	}
 	
 	@Test
 	public void test2()
 	{
+		Log.debug("************************test2 execution Start*********************************************");
 		Assert.assertEquals(true, true);
-		
+		Log.debug("************************test2 execution Complete*************************************************");
 	}
 
 }
